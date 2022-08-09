e38e2af5b39eaa351cda699e703fe9ee8ee56cbe
aliyef510 <imaneliyev315@gmail.com>
2022-08-09 05:58:23 -0700

    responsive desgn

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-
 // NAv -menu toggle menu
-$("#header .bars img ").click(function () {
-    $("#header .bars ul").toggleClass("active");
+jQuery(document).ready(function ($) {
+
+  $("#header .bars img ").click(function () {
+    $("#header .bars ul,body").toggleClass("active");
+
+  })
+
+
 })
-$('body').click(function() {
-    // no need for an if statement here, just use a selector that matches the active elements:
-    $('#header .bars ul.active').removeClass('active');
-    $('body').off('click'); // cancel the body's click handler when it's used
-  });
-  e.stopPropagation(); 
 // NAv -menu toggle menu
