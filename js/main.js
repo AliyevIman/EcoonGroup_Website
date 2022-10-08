e8d3ee9aa9325738771d46f230543bcbc49d1bf3
AliyevIman <imaneliyev315@gmail.com>
2022-10-09 00:50:54 +0400

    a title seo  added

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 "use strict";
-
 // NAv -menu toggle menu
 jQuery(document).ready(function ($) {
-
   $("#header .bars img ").click(function () {
     $("#header .bars ul,body").toggleClass("active");
 
   })
-
-
 })
 // NAv -menu toggle menu
 $(document).ready(() => {
   $('#Top').fadeOut()
 })
-
 $(window).scroll(function(){
-
   if ($(this).scrollTop() > 500) {
     $('#Top').fadeIn();
   } else {
@@ -27,45 +21,30 @@ $('#Top').click(function(){
   $('html, body').animate({scrollTop : 0},800);
   return false;
 });
-
 // Tab Menu
 // $(document).ready(function(){
-
 // 	$('ul.tabs li').click(function(){
 // 		var tab_id = $(this).attr('data-tab');
-
 // 		$('ul.tabs li').removeClass('current');
 // 		$('.tab-content').removeClass('current');
-
 // 		$(this).addClass('current');
 // 		$("#"+tab_id).addClass('current');
 // 	})
-
 // })
-
 // Tab Menu
-
 $(function () {
   $("#projects .images").hide();
   $("#projects .images").first().show();
-
   $("#projects .tab li").on("click", function () {
     let sira = $(this).index();
-
     $("#projects .images").hide();
     $("#projects .images").eq(sira).show();
-
     $("#projects .tab li").removeClass("active");
     $(this).addClass("active");
   });
 });
-
-
-
-
 // Slick
 $(document).ready(function () {
-
   $('.slider').slick({
     dots: true,
     arrows: false,
@@ -105,28 +84,21 @@ $(document).ready(function () {
 
 })
 // Slick
-
 // Footer language tab menu
 $(function () {
   $("#projects .images").first().show();
 
   $("#footer .footer-head .left-footer .left-body ul li p").on("click", function () {
     let sira = $(this).index();
-
-
     $("#footer .footer-head .left-footer .left-body ul li p").removeClass("active");
     $(this).addClass("active");
   });
 });
 // Footer language tab menu
-
 $("a[href='#contact']").click(function () {
   window.scrollTo(0, document.body.scrollHeight);
-
   return false;
 });
-
-
 // if($(window).width()>800){
 //   $("#Top").click(function () {
 //     window.scrollTo({
@@ -137,4 +109,3 @@ $("a[href='#contact']").click(function () {
 //     return false;
 //   });
 // }
-
