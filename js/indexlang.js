bdd9bb3f1992e36b71338cb88dcef6bd1b88a457
AliyevIman <imaneliyev315@gmail.com>
2022-10-11 19:27:15 +0400

    az and rus added to Home

--- js/indexlang.js
+++ js/indexlang.js
@@ -1,24 +1,160 @@
 var myDictionary = {
-    az : {
-        'We Are Building The Future' : 'Biz Gələcəyi Qururuq',
-        'Providing all kind of construction services':'Hər növ tikinti xidmətlərinin verilməsi',
-        'About':"Haqqımızda"
+    az: {
+        'Home': 'Əsas Səhifə',
+        'We Are Building The Future': 'Biz Gələcəyi Qururuq',
+        'Providing all kind of construction services': 'Hər növ tikinti xidmətlərinin verilməsi',
+        'About': "Haqqımızda",
+        'Contact': "Əlaqə",
+        'Gallery': "Qalerya",
+        'Get a Quote': "Bizimlə əlaqə qurun",
+        'Learn more about econgroup': "Econgroup haqqında ətraflı məlumat əldə edin",
+        'WELCOME TO ECONGROUP': "Econ Qrupa Xoş gəldiniz",
+        'Project Collection': "Layihə Kolleksiyası",
+        'We are negotiating on the project, and after reaching anagreement, we are preparingfor its implementation': `Biz layihə ilə bağlı danışıqlar aparır, razılıq əldə etdikdən  sonra isə onun həyata keçirilməsinə başlayırıq`,
+        'Staffing & Recruiting': "Kadrlar və İşə Qəbul",
+        'We recruit qualified personnel for the planned project and, if necessary, train them': "İxtisaslı kadrları işə cəlb edirik planlaşdırılan layihə üçün və əgər lazımdırsa onları öyrədirik",
+        'Delivery & Price': "Çatdırılma və Qiymət",
+        'At the final stage, as the contractor, we delivery the project with maximum effort and fair price': "Son mərhələdə podratçı olaraq layihəni maksimum səy və ədalətli qiymətlə təhvil veririk",
+        'Learn more about econgroup': "Econgroup haqqında ətraflı məlumat əldə edin",
+        'About EconGroup': "EconGroup Haqqında",
+        'People are the key to our success. We strive to hire qualified employees, ensure that they work with us and develop them. We always keep our team in professional shape to provide our customers with highly qualified specialists. We recruit the most experienced staff for any project, and project development is carried out with the most appropriate budget.': "İnsanlar uğurumuzun açarıdır. Biz ixtisaslaşmış işçiləri işə götürməyə, onların bizimlə işləməsini təmin etməyə və onları inkişaf etdirməyə çalışırıq. Müştərilərimizi yüksək ixtisaslı mütəxəssislərlə təmin etmək üçün komandamızı həmişə peşəkar formada saxlayırıq. İstənilən layihə üçün ən təcrübəli işçiləri işə götürür və layihənin hazırlanması ən uyğun büdcə ilə həyata keçiririk.",
+        'See More': "Daha Çoxuna Bax",
+        'What we are offering': "Nə təklif edirik",
+        'Our Services': "Bizim Xidmətlərimiz",
+        // 'Insulation Installing':"İzolyasiya quraşdırılması",
+        // 'Blanket Insulation':"Termal izolyasiyası",
+        // 'Reflective System':"Yansıtıcı sistem",
+        // 'Pipe Insulation':"Boru izolyasiyası",
+        // 'Fibrous':"Lifli",
+        // 'Cellular':"özəkli",
+        // 'Granular':"Qranullu",
+        // 'Welding Production':"Qaynaq istehsalı",
+        'VIEW OUR PROJECTS GALLERY': "LAYİHƏLƏRİMİZƏ GÖZ GƏZDİRİN",
+        'Projects': "Layihələr",
+        'European Construction Group LLC': "European Construction Group MMC",
+        '"European Construction Group" LLC is a collaborative hub that connects subcontractors,contractors, and employees to build a world where good jobs are always available.': `"European Construction Group" MMC subpodratçıları və işçiləri yaxşı işlərin həmişə mövcud olduğu bir dünya qurmaq üçün birləşən əməkdaşlıq mərkəzidir.`,
+        'See All Works': "Bütün layihələrə bax",
+        'See Our Partners': "Tərəfdaşlarımıza göz gəzdirin",
+        'Our Partners': "Tərəfdaşlarımız",
+        'Request Call': "Zəng edin",
+        'Contact Us': "Bizimlə Əlaqə saxlayın",
+        'Interested work in Europe or other world countries Write to us!': "Avropa və ya digər dünya ölkələrində işləmək istəyənlər Bizə yazın!",
+        'Our Office: ': "Ofisimiz",
+    },
+    ru: {
+        'Home': "Главная страница",
+        'Contact': "связь",
+        'About': "о нас",
+        'Gallery': "Галерея",
+        'We Are Building The Future': 'Мы строим будущее',
+        'Providing all kind of construction services': 'Предоставление всех видов строительных услуг',
+        'Get a Quote': "Свяжитесь с нами",
+        'Learn more about econgroup': "Узнать больше об EconGroup",
+        'WELCOME TO ECONGROUP': "ДОБРО ПОЖАЛОВАТЬ В EconGroup",
+        'Project Collection': "Коллекция проектов",
+        'We are negotiating on the project, and after reaching anagreement, we are preparingfor its implementation': `Ведем переговоры по проекту, и после достижения договоренности готовимся к его реализации`,
+        'Staffing & Recruiting': "Персонал и рекрутинг",
+        'We recruit qualified personnel for the planned project and, if necessary, train them': "Подбираем квалифицированный персонал для планируемого проекта и при необходимости обучаем его",
+        'Delivery & Price': "Доставка и цена",
+        'At the final stage, as the contractor, we delivery the project with maximum effort and fair price': "На финальном этапе, как подрядчик, мы сдаем проект с максимальными усилиями и справедливой ценой.",
+        'Learn more about econgroup': "Узнать больше об EconGroup",
+        'About EconGroup': "О компании EconGroup",
+        'People are the key to our success. We strive to hire qualified employees, ensure that they work with us and develop them. We always keep our team in professional shape to provide our customers with highly qualified specialists. We recruit the most experienced staff for any project, and project development is carried out with the most appropriate budget.': "Люди – ключ к нашему успеху. Мы стремимся нанимать, удерживать и развивать квалифицированных работников. Мы всегда держим нашу команду в профессиональной форме, чтобы обеспечить наших клиентов высококвалифицированными специалистами. Мы нанимаем самых опытных работников для любого проекта и реализуем разработку проекта с наиболее подходящим бюджетом.",
+        'See More': "Узнать больше",
+        'What we are offering': "Что мы предлагаем",
+        'Our Services': "Наши услуги",
+        // 'Insulation Installing':"İzolyasiya quraşdırılması",
+        // 'Blanket Insulation':"Termal izolyasiyası",
+        // 'Reflective System':"Yansıtıcı sistem",
+        // 'Pipe Insulation':"Boru izolyasiyası",
+        // 'Fibrous':"Lifli",
+        // 'Cellular':"özəkli",
+        // 'Granular':"Qranullu",
+        // 'Welding Production':"Qaynaq istehsalı",
+        'VIEW OUR PROJECTS GALLERY': "ПОСЕТИТЕ НАШИ ПРОЕКТЫ",
+        'Projects': "Проекты",
+        'European Construction Group LLC': "European Construction Group MMC",
+        '"European Construction Group" LLC is a collaborative hub that connects subcontractors,contractors, and employees to build a world where good jobs are always available.': `"European Construction Group LLC — это центр сотрудничества, где субподрядчики и сотрудники объединяются, чтобы построить мир, в котором всегда есть хорошие рабочие места.`,
+        'See All Works': "Посмотреть все проекты",
+        'See Our Partners': "Просмотрите наших партнеров",
+        'Our Partners': "Наши партнеры",
+        'Request Call': "Позвонить",
+        'Contact Us': "Свяжитесь с нами",
+        'Interested work in Europe or other world countries Write to us!': "Те, кто хочет работать в Европе или других странах мира, пишите нам!",
+        'Our Office: ': "Наш офис",
+
+        
     }
 }
 $.tr.dictionary(myDictionary);
 
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     // change the language
-    $('#language').change(function() {
+    $('#language').change(function () {
         $.tr.language($(this).val());
         var tr = $.tr.translator();
+        // -------------AZ-----------
         $('#about').text(tr('About'));
+        $('#contact').text(tr('Contact'));
+        $('#gallery').text(tr('Gallery'));
         $('#welcome').text(tr('We Are Building The Future'));
         $('#providing').text(tr('Providing all kind of construction services'));
+        $('#getaquote').text(tr('Get a Quote'));
+        $('#ecocngrouplearn').text(tr('Learn more about econgroup'));
+        $('#WelcomeEcon').text(tr('WELCOME TO ECONGROUP'));
+        $('#ProjectCollection').text(tr('Project Collection'));
+        $('#ProjectContent').text(tr('We are negotiating on the project, and after reaching anagreement, we are preparingfor its implementation'));
+        $('#Staffing').text(tr('Staffing & Recruiting'));
+        $('#Werecruit').text(tr('We recruit qualified personnel for the planned project and, if necessary, train them'));
+        $('#Delivery').text(tr('Delivery & Price'));
+        $('#DeliveryContent').text(tr('At the final stage, as the contractor, we delivery the project with maximum effort and fair price'));
+        $('#ecocngrouplearnn').text(tr('Learn more about econgroup'));
+        $('#AboutEconGroup').text(tr('About EconGroup'));
+        $('#AboutContent').text(tr('People are the key to our success. We strive to hire qualified employees, ensure that they work with us and develop them. We always keep our team in professional shape to provide our customers with highly qualified specialists. We recruit the most experienced staff for any project, and project development is carried out with the most appropriate budget.'));
+        $('#SeeMore').text(tr('See More'));
+        $('#Offering').text(tr('What we are offering'));
+        $('#OurService').text(tr('Our Services'));
+        // $('#Insulation').text(tr('Insulation Installing'));
+        // $('#Insulation1').text(tr('Blanket Insulation'));
+        // $('#Insulation2').text(tr('Reflective System'));
+        // $('#Insulation3').text(tr('Pipe Insulation'));
+        // $('#Insulation4').text(tr('Fibrous'));
+        // $('#Insulation5').text(tr('Cellular'));
+        // $('#Insulation6').text(tr('Granular'));
+        // $('#Welding').text(tr('Welding Production'));
+        // $('#Welding1').text(tr('Welding Production'));
+        // $('#Welding2').text(tr('Welding Production'));
+        // $('#Welding3').text(tr('Welding Production'));
+
+        // Gallery
+        $('#ViewProjects').text(tr('VIEW OUR PROJECTS GALLERY'));
+        $('#Projects').text(tr('Projects'));
+        $('#SeeAll').text(tr('See All Works'));
+        // European 
+        $('#European').text(tr('European Construction Group LLC'));
+        $('#EuropeanContent').text(tr(`"European Construction Group" LLC is a collaborative hub that connects subcontractors,contractors, and employees to build a world where good jobs are always available.`));
+        // Partners
+        $('#SeePartner').text(tr('See Our Partners'));
+        $('#Ourpartner').text(tr('Our Partners'));
+        // Footer .........
+        $('#RequestCall').text(tr('Request Call'));
+        $('#ContactUs').text(tr('Contact Us'));
+        $('#InterestedWork').text(tr('Interested work in Europe or other world countries Write to us!'));
+        $('#OurOffice').text(tr('Our Office: '));
+        $('#Home').text(tr('Home'));
+        $('#').text(tr(''));
+        $('#').text(tr(''));
+        $('#').text(tr(''));
+        $('#').text(tr(''));
+        $('#').text(tr(''));
+        $('#').text(tr(''));
+
+
     });
     // default language is English if no cookies present
     var language = $.tr.language('en', true);
+
     $('#language').val(language);
     $('#language').change();
 });
